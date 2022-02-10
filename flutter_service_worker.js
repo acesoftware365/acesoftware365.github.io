80571fb3071ef9e75a2d861c386c12270b6678e3
acesoftware365 <74153117+acesoftware365@users.noreply.github.com>
2022-02-10 17:28:32 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "c51d726642d98b6db9182b395c6da87c",
 "icons/Icon-maskable-192.png": "f471083dbddb634bf9941a59193169c1",
 "icons/Icon-maskable-512.png": "c51d726642d98b6db9182b395c6da87c",
-"index.html": "62992a6e0d9cf6526adec89d42d9b740",
-"/": "62992a6e0d9cf6526adec89d42d9b740",
-"main.dart.js": "d6a33b1bdd82e03df67a0d4c8d2676f5",
+"index.html": "0f82d0404f5e8770785ed9c34f2db2f9",
+"/": "0f82d0404f5e8770785ed9c34f2db2f9",
+"main.dart.js": "a181834bdf3aefd287fd4892a7745b08",
 "manifest.json": "b0d8c2e7a3af596bdbcb5844ceed937e",
 "version.json": "ed54420cb3c0d28d454f399850d22d0c"
 };
